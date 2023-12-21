0e0c06beae05a06d1b652b6b04d783b5d20b26aa
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2023-12-21 14:49:17 -0600

    Ajuste en apis

--- backendImprenta/settings/local.py
+++ backendImprenta/settings/local.py
@@ -12,7 +12,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': '5194',
+        'PASSWORD': 'cesaradmin',
         'NAME':'imprentaGarcia',
         'OPTIONS':{
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
