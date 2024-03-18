358d231437d8ed90f4f5ee09a3e06bb2c75891ed
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2024-03-18 16:35:47 -0600

    Ajustes en reglas de negocio

--- apps/Terminado/models.py
+++ apps/Terminado/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Terminado(models.Model):
     idTerminado = models.AutoField(auto_created=True, primary_key=True)
     nombre = models.CharField(max_length=50, blank=False, null=False)
-    cantidad = models.IntegerField(blank=False, null=False)
+    distancia = models.IntegerField(blank=False, null=False)
     precio = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
     tipoTrabajo = models.CharField(
         max_length=20,
